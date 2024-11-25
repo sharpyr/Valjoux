3898a727cd76f5145207ffb1162e6ced70111cbe
hoyeungw <hoyeungw@outlook.com>
2024-11-25 08:38:26 +0800

    patch version, update dependencies

--- Valjoux.Strategies/Utils/DataFactory.cs
+++ Valjoux.Strategies/Utils/DataFactory.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using Analys;
 using Veho;
-using Veho.List;
 using Veho.Vector;
 
 namespace Valjoux.Utils {
   public static class DataFactory {
     public static Table<double> Init(List<(string name, Action action)> methods) => Table<double>.Build(
-      methods.Map(kv => kv.name).ToArray(),
+      methods.Map(kv => kv.name),
       Vec.Iso<double>(methods.Count, 0).ToRow()
     );
     public static (Crostab<double> elapsed, Crostab<TO> result) Init<T, TO>(
@@ -16,8 +15,8 @@ namespace Valjoux.Utils {
       List<(string key, T value)> parameters
     ) {
       string[]
-        m = methods.Map(kv => kv.name).ToArray(),
-        p = parameters.Map(kv => kv.key).ToArray();
+        m = methods.Map(kv => kv.name),
+        p = parameters.Map(kv => kv.key);
       return (Crostab<double>.Build(p, m), Crostab<TO>.Build(p, m));
     }
   }
